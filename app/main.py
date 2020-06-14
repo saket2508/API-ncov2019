8c79c5f6bbf7ab62c5d8c81043bc5a52bd19e991
saket sn.3 <saketns@gmail.com>
2020-06-14 08:19:54 +0530

    Initial commit

--- app/main.py
+++ app/main.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-        return getCurrentData()
+        return "<h1>Welcome to my API</h1>"
 
+@app.route("/latest")
+def latest_view():
+        return getCurrentData()
 @app.route("/yesterday")
 def yesterday_view():
         return getPreviousDayData()
